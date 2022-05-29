98addfe46daf36739fd3f980ab5a4fececeba7ea
jayhuang <shh031@ucsd.edu>
2022-05-29 15:44:01 -0700

    update disfuser_server

--- tribbler/src/disfuser_server.rs
+++ tribbler/src/disfuser_server.rs
@@ -1,6 +1,9 @@
 use crate::disfuser::disfuser_server::{self, Disfuser};
 use crate::disfuser::{
-    Create, CreateReply, LookUp, Read, Reply, Unlink, UnlinkReply, Write, WriteReply,
+    Access, AccessReply, Create, CreateReply, Getattr, GetattrReply, Getxattr, GetxattrReply,
+    Listxattr, ListxattrReply, LookUp, Open, OpenReply, Read, Release, ReleaseReply, Rename,
+    RenameReply, Reply, Setattr, SetattrReply, Setxattr, SetxattrReply, Unlink, UnlinkReply, Write,
+    WriteReply,
 };
 use crate::error::SUCCESS;
 use crate::storage::FileRequest;
@@ -17,6 +20,8 @@ use tokio_stream::Stream;
 use tokio_stream::StreamExt;
 use tonic::{Response, Status};
 type readStream = Pin<Box<dyn Stream<Item = Result<Reply, Status>> + Send>>;
+type getxattrStream = Pin<Box<dyn Stream<Item = Result<GetxattrReply, Status>> + Send>>;
+type listxattrStream = Pin<Box<dyn Stream<Item = Result<ListxattrReply, Status>> + Send>>;
 // type readStream = Pin<Box<dyn Stream<Item = Result<Read, Status>> + Send>>;
 // type lookupStream = Pin<Box<dyn Stream<Item = Result<LookUp, Status>> + Send>>;
 pub const slice_size: usize = 128;
@@ -48,7 +53,30 @@ fn reply_response_iter(msg: String, errcode: i32) -> Vec<Reply> {
         };
         vec.push(element.clone());
     }
+    return vec;
+}
 
+fn getxattr_response_iter(msg: String, size: u32, errcode: i32) -> Vec<GetxattrReply> {
+    let data_len = msg.len();
+    let mut n = data_len / slice_size;
+    if data_len % slice_size != 0 {
+        n += 1;
+    }
+
+    let mut vec: Vec<GetxattrReply> = Vec::new();
+    let mut start = 0;
+    let mut end = 0;
+
+    for i in 0..n {
+        start = i * slice_size;
+        end = min(start + slice_size, data_len);
+        let element = GetxattrReply {
+            data: msg.clone()[start..end].to_string(),
+            size: size,
+            errcode: errcode,
+        };
+        vec.push(element.clone());
+    }
     return vec;
 }
 
@@ -63,6 +91,8 @@ impl DisfuserServer {
 #[async_trait]
 impl Disfuser for DisfuserServer {
     type readStream = readStream;
+    type getxattrStream = getxattrStream;
+    type listxattrStream = listxattrStream;
 
     async fn read(
         &self,
@@ -277,5 +307,225 @@ impl Disfuser for DisfuserServer {
             Ok(value) => Ok(Response::new(UnlinkReply { errcode: value })),
             Err(_) => Err(Status::invalid_argument("unlink failed")),
         }
+    }
+
+    async fn getattr(
+        &self,
+        request: tonic::Request<Getattr>,
+    ) -> Result<tonic::Response<GetattrReply>, tonic::Status> {
+        let request_inner = request.into_inner();
+        let mut file_request = FileRequest {
+            uid: request_inner.frequest.clone().uid,
+            gid: request_inner.frequest.clone().gid,
+            pid: request_inner.frequest.clone().pid,
+        };
+        let result = self
+            .filesystem
+            .getattr(&file_request, request_inner.ino)
+            .await;
+
+        // change fileAttr to string
+        match result {
+            Ok(value) => Ok(Response::new(GetattrReply {
+                file_attr: serde_json::to_string(&value.0.clone().unwrap()).unwrap(),
+                errcode: value.1,
+            })),
+            Err(_) => Err(Status::invalid_argument("getattr failed")),
+        }
+    }
+
+    async fn open(
+        &self,
+        request: tonic::Request<Open>,
+    ) -> Result<tonic::Response<OpenReply>, tonic::Status> {
+        let request_inner = request.into_inner();
+        let mut file_request = FileRequest {
+            uid: request_inner.frequest.clone().uid,
+            gid: request_inner.frequest.clone().gid,
+            pid: request_inner.frequest.clone().pid,
+        };
+        let result = self
+            .filesystem
+            .open(&file_request, request_inner.ino, request_inner.flags)
+            .await;
+
+        match result {
+            Ok(value) => Ok(Response::new(OpenReply {
+                fh: value.0.clone().unwrap().0,
+                openflag: value.0.clone().unwrap().1,
+                errcode: value.1,
+            })),
+            Err(_) => Err(Status::invalid_argument("open failed")),
+        }
+    }
+    async fn release(
+        &self,
+        request: tonic::Request<Release>,
+    ) -> Result<tonic::Response<ReleaseReply>, tonic::Status> {
+        let request_inner = request.into_inner();
+        let mut file_request = FileRequest {
+            uid: request_inner.frequest.clone().uid,
+            gid: request_inner.frequest.clone().gid,
+            pid: request_inner.frequest.clone().pid,
+        };
+        let result = self
+            .filesystem
+            .release(
+                &file_request,
+                request_inner.ino,
+                request_inner.fh,
+                request_inner.flags,
+                request_inner.lock_owner,
+                request_inner.flush,
+            )
+            .await;
+
+        match result {
+            Ok(value) => Ok(Response::new(ReleaseReply { errcode: value })),
+            Err(_) => Err(Status::invalid_argument("release failed")),
+        }
+    }
+    async fn setxattr(
+        &self,
+        request: tonic::Request<tonic::Streaming<Setxattr>>,
+    ) -> Result<tonic::Response<SetxattrReply>, tonic::Status> {
+        let mut in_stream = request.into_inner();
+
+        let mut file_request: FileRequest = FileRequest {
+            uid: 0,
+            gid: 0,
+            pid: 0,
+        };
+        let mut inode: u64 = 0;
+        let mut name: String = "".to_string();
+        let mut value_data: Vec<String> = Vec::new();
+        let mut flags: i32 = 0;
+        let mut position: u32 = 0;
+        // let mut stream = in_stream.take(0);
+        while let item = in_stream.next().await {
+            match item {
+                Some(value) => {
+                    let v = value.unwrap();
+
+                    value_data.push(v.value);
+                    file_request = FileRequest {
+                        uid: v.frequest.clone().uid,
+                        gid: v.frequest.clone().gid,
+                        pid: v.frequest.clone().pid,
+                    };
+                    inode = v.ino;
+                    name = v.name;
+                    flags = v.flags;
+                    position = v.position;
+                }
+                None => {}
+            }
+        }
+        let joined_data = value_data.join("");
+        let data = serde_json::from_str::<&[u8]>(&joined_data).unwrap();
+
+        let mut osstring = OsString::new();
+        osstring.push(name);
+
+        let result = self
+            .filesystem
+            .setxattr(
+                &file_request,
+                inode,
+                &osstring.as_os_str(),
+                data,
+                flags,
+                position,
+            )
+            .await;
+
+        match result {
+            Ok(value) => Ok(Response::new(SetxattrReply { errcode: value })),
+            Err(_) => Err(Status::invalid_argument("setxattr failed")),
+        }
+    }
+
+    async fn getxattr(
+        &self,
+        request: tonic::Request<Getxattr>,
+    ) -> Result<tonic::Response<Self::getxattrStream>, tonic::Status> {
+        let r_inner = request.into_inner();
+
+        let request = FileRequest {
+            uid: r_inner.frequest.clone().uid,
+            gid: r_inner.frequest.clone().gid,
+            pid: r_inner.frequest.clone().pid,
+        };
+
+        let mut osstring = OsString::new();
+        osstring.push(r_inner.name);
+
+        let result = self
+            .filesystem
+            .getxattr(&request, r_inner.ino, &osstring.as_os_str(), r_inner.size)
+            .await;
+        let mut reply = Vec::new();
+        match result {
+            Ok(value) => {
+                if value.1 != SUCCESS {
+                    reply = vec![GetxattrReply {
+                        data: "".to_string(),
+                        size: 0,
+                        errcode: value.1,
+                    }];
+                } else {
+                    // divide the message into appropriate size, and make a vector
+                    let content = value.0.clone().unwrap().0;
+                    let size = value.0.clone().unwrap().1;
+                    reply = getxattr_response_iter(content, size, SUCCESS);
+                }
+            }
+            Err(_) => {}
+        };
+
+        let mut stream = Box::pin(tokio_stream::iter(reply).throttle(Duration::from_millis(200)));
+        let (tx, rx) = mpsc::channel(128);
+
+        tokio::spawn(async move {
+            while let Some(item) = stream.next().await {
+                match tx.send(Result::<_, Status>::Ok(item)).await {
+                    Ok(_) => {
+                        // item (server response) was queued to be send to client
+                    }
+                    Err(_item) => {
+                        // output_stream was build from rx and both are dropped
+                        break;
+                    }
+                }
+            }
+            println!("\tclient disconnected");
+        });
+
+        let output_stream = ReceiverStream::new(rx);
+        Ok(Response::new(
+            Box::pin(output_stream) as Self::getxattrStream
+        ))
+    }
+
+    async fn listxattr(
+        &self,
+        request: tonic::Request<Listxattr>,
+    ) -> Result<tonic::Response<Self::listxattrStream>, tonic::Status> {
+    }
+    async fn access(
+        &self,
+        request: tonic::Request<Access>,
+    ) -> Result<tonic::Response<AccessReply>, tonic::Status> {
+    }
+    async fn rename(
+        &self,
+        request: tonic::Request<Rename>,
+    ) -> Result<tonic::Response<RenameReply>, tonic::Status> {
+    }
+
+    async fn setattr(
+        &self,
+        request: tonic::Request<Setattr>,
+    ) -> Result<tonic::Response<SetattrReply>, tonic::Status> {
     }
 }
